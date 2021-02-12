e325dfd15980ba89d701b1da9689d7e11d8275f0
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2021-02-12 17:19:43 -0300

    create array on service and imp on controller

--- backend/src/holidays/holidays.controller.ts
+++ backend/src/holidays/holidays.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HolidaysService } from './holidays.service';
 
 @Controller('holidays')
 export class HolidaysController {
   constructor(private holidaysService: HolidaysService) {}
+
+  @Get()
+  getAllHolidays() {
+    return this.holidaysService.getAllHolidays();
+  }
 }
--- backend/src/holidays/holidays.service.ts
+++ backend/src/holidays/holidays.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HolidaysService {}
+export class HolidaysService {
+  private holidays = [];
+
+  getAllHolidays() {
+    return this.holidays;
+  }
+}
