227a84c817ebfc8d02dfb9832c39526e24008999
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2021-02-12 17:15:49 -0300

    create service and add on controller holidays

--- backend/src/holidays/holidays.controller.ts
+++ backend/src/holidays/holidays.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { HolidaysService } from './holidays.service';
 
 @Controller('holidays')
-export class HolidaysController {}
+export class HolidaysController {
+  constructor(private holidaysService: HolidaysService) {}
+}
