c7e0348757f40930da4698931f713c85a56155e5
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2021-02-12 17:52:20 -0300

    post on services and controller

--- backend/src/holidays/holidays.controller.ts
+++ backend/src/holidays/holidays.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { HolidaysService } from './holidays.service';
 
 @Controller('holidays')
@@ -8,5 +8,10 @@ export class HolidaysController {
   @Get()
   getAllHolidays() {
     return this.holidaysService.getAllHolidays();
+  }
+
+  @Post()
+  createHoliday(@Body() body) {
+    console.log('body', body);
   }
 }
--- backend/src/holidays/holidays.service.ts
+++ backend/src/holidays/holidays.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { Holiday, HolidaysType } from './holidays.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class HolidaysService {
@@ -6,5 +8,17 @@ export class HolidaysService {
 
   getAllHolidays() {
     return this.holidays;
+  }
+
+  createHoliday(name: string, date: Date) {
+    const holiday: Holiday = {
+      id: uuidv4(),
+      name,
+      date,
+      type: HolidaysType.NACIONAL,
+    };
+
+    this.holidays.push(holiday);
+    return holiday;
   }
 }
